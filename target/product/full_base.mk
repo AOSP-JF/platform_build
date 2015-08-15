c2661b1028c6b4a800dc30a9fd01eb65171075f3
Ricardo Cerqueira <ricardo@cyngn.com>
2015-08-15 15:29:29 +0100

    product: Let a device customize its own locales
    Use TARGET_LOCALES. If existing, it'll override the defaults except
    for en_US
    
    Change-Id: I450fa5023ba48f07622d23158012b5b7d5f2130b

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -47,8 +47,12 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 
+ifeq ($(TARGET_LOCALES),)
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+else
+PRODUCT_LOCALES := $(TARGET_LOCALES)
+endif
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
